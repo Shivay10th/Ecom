77347ca6b1fdc7ad9451a9f6e627c46912fcb7f4
Shivam Yadav <shivay9455@gmail.com>
2022-02-09 18:08:22 +0530

    Added add product functionality

--- projectfrontend/src/admin/helper/adminapicall.js
+++ projectfrontend/src/admin/helper/adminapicall.js
@@ -26,6 +26,7 @@ export const getCategories = () => {
 
 // Products call
 
+//create product
 export const createProduct = (userId, token, product) => {
 	return fetch(`${API}/product/create/${userId}`, {
 		method: 'POST',
@@ -39,8 +40,43 @@ export const createProduct = (userId, token, product) => {
 		.catch(console.log);
 };
 
+//get all products
 export const getAllProducts = () => {
 	return fetch(`${API}/products`, { method: 'GET' })
+		.then((res) => res.json())
+		.catch(console.log);
+};
+
+// get a product
+export const getAProduct = (productId) => {
+	return fetch(`${API}/product/${productId}`, { method: 'GET' })
+		.then((res) => res.json())
+		.catch(console.log);
+};
+
+//update a product
+export const updateAProduct = (productId, userId, token, product) => {
+	return fetch(`${API}/product/${productId}/${userId}`, {
+		method: 'PUT',
+		headers: {
+			Accept: 'application/json',
+			Authorization: `Bearer ${token}`,
+		},
+		body: product,
+	})
+		.then((res) => res.json())
+		.catch(console.log);
+};
+
+//delete a product
+export const deleteAProduct = (productId, userId, token) => {
+	return fetch(`${API}/product/${productId}/${userId}`, {
+		method: 'DELETE',
+		headers: {
+			Accept: 'application/json',
+			Authorization: `Bearer ${token}`,
+		},
+	})
 		.then((res) => res.json())
 		.catch(console.log);
 };
